457acc831555b4e10e3d5f1007d3138e31ae07b8
Jooyoung Jang <84753951+0neand0nly@users.noreply.github.com>
2023-01-19 16:04:16 +0900

    feat: Web Crawler for CWE

---
+++ src/main/java/Main.java
@@ -0,0 +1,49 @@
+package main.java;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException
+    {
+       String URL ="https://cwe.mitre.org/data/definitions/22.html";
+       Document doc = Jsoup.connect(URL).get();
+       String filename="testing.txt";
+       File file = new File(filename);
+       FileOutputStream fos = new FileOutputStream(file);
+
+
+
+        Elements elem = doc.select("div[class=\"top\"]");
+        for(Element e: elem.select("div")) {
+
+            for(int i=0;i<e.text().length();i++)
+            {
+                System.out.print(e.text().charAt(i));
+                fos.write(e.text().charAt(i));
+
+                if(e.text().charAt(i) == ';'||e.text().charAt(i) == '}'||e.text().charAt(i) == '{')
+                {
+
+                    System.out.println();
+                    fos.write('\n');
+                }
+            }
+
+
+            String linediv ="_____________________________________\n\n\n";
+            System.out.print(linediv);
+            for(int idx =0 ;idx<linediv.length();idx++)
+            {
+                fos.write(linediv.charAt(idx));
+            }
+        }
+        fos.close();
+    }
+
+
+}
