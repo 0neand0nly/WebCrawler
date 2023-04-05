d675695c60b4d0f1e82e2952243aa349aec8faa7
Jooyoung Jang <84753951+0neand0nly@users.noreply.github.com>
2023-04-05 14:03:01 +0900

    Crawler for Introduction to Programming
    Converted demo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,38 +6,69 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String URL = "https://cwe.mitre.org/data/definitions/400.html";
-        Document doc = Jsoup.connect(URL).get();
-        String filename = "testing.txt";
+        ArrayList<String> buff = new ArrayList<String>();
+
+
+        Scanner s = new Scanner(System.in);
+        System.out.printf("Type in the Address of Moss anaysis result: ");
+        String str = s.nextLine();
+        String line="";
+
+        Document doc = Jsoup.connect(str).get();
+        String filename = "T_results.csv";
         File file = new File(filename);
         FileOutputStream fos = new FileOutputStream(file);
 
+        BufferedWriter bw = new BufferedWriter((new OutputStreamWriter(fos)));
 
-        Elements elem = doc.select("div[class=\"top\"]");
+
+        int count =0;
+        Elements elem = doc.select("tr");
         for (Element e : elem) {
 
             for (int i = 0; i < e.text().length(); i++) {
-                System.out.print(e.text().charAt(i));
-                fos.write(e.text().charAt(i));
+               // System.out.print(e.text().charAt(i));
+                //fos.write(e.text().charAt(i));
+                line+= e.text().charAt(i);
+                if (e.text().charAt(i) == ')' || e.text().charAt(i) == '}' || e.text().charAt(i) == '{') {
 
-                if (e.text().charAt(i) == ';' || e.text().charAt(i) == '}' || e.text().charAt(i) == '{') {
-
-                    System.out.println();
-                    fos.write('\n');
+                   // line+= e.text().charAt(i);
+                    line += ',';
+                    //System.out.println();
+                    //fos.write(',');
+                    //fos.write('\n');
                 }
             }
+            count ++;
+
+            /*if(count%3==0 ){
+                line += '\n';
+            }else {
+                line += ',';
+            }*/
+           // line+=',';
+            line +='\n';
+            buff.add(line);
+            bw.write(line);
+            line ="";
+
 
 
             String linediv = "\n_____________________________________\n\n";
-            System.out.print(linediv);
-            for (int idx = 0; idx < linediv.length(); idx++) {
-                fos.write(linediv.charAt(idx));
-            }
+            //System.out.print(linediv);
+
+
 
         }
+        bw.flush();
+        bw.close();
         //System.out.println(elem.text());
         fos.close();
     }
